a8de32626ac6c3fb20f21c2f1a34d4539f31002e
Gerardo Loyo <gerardo.loyo@srax.com>
2023-03-24 00:28:36 -0700

    Added config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+# config.py
+
+class Config:
+    SECRET_KEY = 'your-secret-key'
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///development.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///testing.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///production.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
