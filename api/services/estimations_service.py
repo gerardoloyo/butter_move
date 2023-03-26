4ad676f3e17db1f6ecf377d5b3b8998e5fb173ee
Gerardo Loyo <gerardo.loyo@srax.com>
2023-03-25 18:37:53 -0700

    All to upper case

--- api/services/estimations_service.py
+++ api/services/estimations_service.py
@@ -3,21 +3,24 @@ import re
 from typing import Dict
 
 STATE_COMMISSIONS = {
-    'NY': {'Normal': 0.25, 'Premium': 0.35},
-    'CA': {'Normal': 0.23, 'Premium': 0.33},
-    'AZ': {'Normal': 0.20, 'Premium': 0.30},
-    'TX': {'Normal': 0.18, 'Premium': 0.28},
-    'OH': {'Normal': 0.15, 'Premium': 0.25}
+    'NY': {'NORMAL': 0.25, 'PREMIUM': 0.35},
+    'CA': {'NORMAL': 0.23, 'PREMIUM': 0.33},
+    'AZ': {'NORMAL': 0.20, 'PREMIUM': 0.30},
+    'TX': {'NORMAL': 0.18, 'PREMIUM': 0.28},
+    'OH': {'NORMAL': 0.15, 'PREMIUM': 0.25}
 }
 
 def calculate_estimation(state: str, estimation_type: str, kilometers: float, base_amount: float) -> Dict[str, str]:
+    state = state.upper()
+    estimation_type = estimation_type.upper()
+
     if state not in STATE_COMMISSIONS:
         raise ValueError('Unsupported state')
 
     commission = STATE_COMMISSIONS[state][estimation_type]
     total_amount = base_amount * (1 + commission)
 
-    if estimation_type == 'Normal':
+    if estimation_type == 'NORMAL':
         if state == 'NY':
             total_amount *= 1.21
         elif state in ('CA', 'AZ') and kilometers > 26:
@@ -27,7 +30,7 @@ def calculate_estimation(state: str, estimation_type: str, kilometers: float, ba
                 total_amount *= 0.97
             elif kilometers > 30:
                 total_amount *= 0.95
-    elif estimation_type == 'Premium':
+    elif estimation_type == 'PREMIUM':
         if kilometers > 25:
             total_amount *= 0.95
 
