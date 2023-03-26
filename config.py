b9f9d1f75583c697b64130dbbc06059dc2393daf
Gerardo Loyo <gerardo.loyo@srax.com>
2023-03-25 18:34:17 -0700

    Just one environment for the moment

--- config.py
+++ config.py
@@ -1,19 +1,6 @@
+import os
+
 class Config:
     SECRET_KEY = 'your-secret-key'
-
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///development.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-class TestingConfig(Config):
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///testing.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///production.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
