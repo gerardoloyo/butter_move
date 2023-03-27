eb236b7092002dfdf1d27c67f13dd0fe2701da4c
Gerardo Loyo <gerardo.loyo@srax.com>
2023-03-26 19:28:05 -0700

    +Added States model properties to make generic estimation formula

--- api/models/states_model.py
+++ api/models/states_model.py
@@ -5,6 +5,10 @@ class State(db.Model):
     abbreviation = db.Column(db.String(2), unique=True, nullable=False)
     normal_commission = db.Column(db.Float, nullable=False)
     premium_commission = db.Column(db.Float, nullable=False)
+    iva = db.Column(db.float, nullable=False)
+    base_discount = db.Column(db.float, nullable=False)
+    total_discount = db.Column(db.float, nullable=False)
+    premium_discount = db.Column(db.JSON, nullable=False)
 
     def __repr__(self):
         return f"State('{self.abbreviation}', {self.normal_commission}, {self.premium_commission})"
