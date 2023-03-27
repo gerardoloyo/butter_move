782d5986d73b947cbea64997be6b5a853626ce22
Gerardo Loyo <gerardo.loyo@srax.com>
2023-03-26 20:35:38 -0700

    +Added new columns to read discount values and km range

--- api/models/states_model.py
+++ api/models/states_model.py
@@ -6,8 +6,8 @@ class State(db.Model):
     normal_commission = db.Column(db.Float, nullable=False)
     premium_commission = db.Column(db.Float, nullable=False)
     iva = db.Column(db.float, nullable=False)
-    base_discount = db.Column(db.float, nullable=False)
-    total_discount = db.Column(db.float, nullable=False)
+    base_discount = db.Column(db.JSON, nullable=False)
+    total_discount = db.Column(db.JSON, nullable=False)
     premium_discount = db.Column(db.JSON, nullable=False)
 
     def __repr__(self):
--- app.py
+++ app.py
@@ -9,11 +9,51 @@ def create_initial_states():
 
     if State.query.count() == 0:
         initial_states = [
-            State(abbreviation='NY', normal_commission=0.25, premium_commission=0.35),
-            State(abbreviation='CA', normal_commission=0.23, premium_commission=0.33),
-            State(abbreviation='AZ', normal_commission=0.20, premium_commission=0.30),
-            State(abbreviation='TX', normal_commission=0.18, premium_commission=0.28),
-            State(abbreviation='OH', normal_commission=0.15, premium_commission=0.25)
+            State(
+                abbreviation='NY',
+                normal_commission=0.25,
+                premium_commission=0.35,
+                iva=0.21,
+                base_discount={'value': 0.0}, 
+                total_discount={'value': 0.0}, 
+                premium_discount={'value': 0.05, 'min': 25.0, 'min_bound': 'inclusive', 'max': 'inf', 'max_boundary': 'exclusive'}
+            ),
+            State(
+                abbreviation='CA',
+                normal_commission=0.23, 
+                premium_commission=0.33,
+                iva=0.0
+                base_discount={'value': 0.0}, 
+                total_discount={'value': 0.05, 'min': 26.0, 'min_bound': 'exclusive', 'max': 'inf', 'max_boundary': 'exclusive'}, 
+                premium_discount={'value': 0.95, 'min': 25.0, 'min_bound': 'inclusive', 'max': 'inf', 'max_boundary': 'exclusive'}
+            ),
+            State(
+                abbreviation='AZ',
+                normal_commission=0.20,
+                premium_commission=0.30,
+                iva=0.0
+                base_discount={'value': 0.0}, 
+                total_discount={'value': 0.05, 'min': 26.0, 'min_bound': 'exclusive', 'max': 'inf', 'max_boundary': 'exclusive'}, 
+                premium_discount={'value': 0.95, 'min': 25.0, 'min_bound': 'inclusive', 'max': 'inf', 'max_boundary': 'exclusive'}
+            ),
+            State(
+                abbreviation='TX',
+                normal_commission=0.18,
+                premium_commission=0.28,
+                iva=0.0
+                base_discount={'value': 0.03, 'min': 20.0, 'min_bound': 'inclusive', 'max': 30.0, 'max_boundary': 'inclusive'}, 
+                total_discount={'value': 0.05, 'min': 30.0, 'min_bound': 'exclusive', 'max': 'inf', 'max_boundary': 'exclusive'}, 
+                premium_discount={'value': 0.95, 'min': 25.0, 'min_bound': 'inclusive', 'max': 'inf', 'max_boundary': 'exclusive'}
+            ),
+            State(
+                abbreviation='OH',
+                normal_commission=0.15,
+                premium_commission=0.25,
+                iva=0.0
+                base_discount={'value': 0.03, 'min': 20.0, 'min_bound': 'inclusive', 'max': 30.0, 'max_boundary': 'inclusive'}, 
+                total_discount={'value': 0.05, 'min': 30.0, 'min_bound': 'exclusive', 'max': 'inf', 'max_boundary': 'exclusive'}, 
+                premium_discount={'value': 0.95, 'min': 25.0, 'min_bound': 'inclusive', 'max': 'inf', 'max_boundary': 'exclusive'}
+            )
         ]
 
         for state in initial_states:
