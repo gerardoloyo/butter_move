26f4ff539316215ac43d64d808f6d5ca2e9a5e54
Gerardo Loyo <gerardo.loyo@srax.com>
2023-03-26 17:24:10 -0700

    -Remove unused import

--- api/controllers/estimations_controller.py
+++ api/controllers/estimations_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request
 from api.validators import validators
 from api.services.estimations_service import EstimationsService
 
